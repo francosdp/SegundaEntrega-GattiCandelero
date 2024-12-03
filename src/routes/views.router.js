0516acd34b8393798e063a068101c451e9f00d3a
Franco Gatti <francogatti.sdp@outlook.com>
2024-12-03 13:18:18 -0300

    Seccion home.handlebars completada

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,9 +1,28 @@
 import express from 'express'
+import fs from 'fs/promises'
+import path from 'path'
+
+let products =[]
+
+async function readJson() {  
+    try {  
+        const productFilePath = path.resolve('data', 'productos.json')
+        const data = await fs.readFile(productFilePath, 'utf8');   
+        products = JSON.parse(data);
+        console.log(products); 
+        
+    } catch (error) {  
+        console.error(error);  
+    }  
+    
+}  
+
+readJson();
 
 const router = express.Router()
 
-router.get('/', (req,res)=>{
-    res.render('index',{})
+router.get('/', (req, res) => {
+    res.render('home', { products })
 })
 
 export default router
